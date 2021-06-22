096bedf041213d3a77681c5bb38521e916cdff4a
aroyehunahmad <ahmadadewale800@gmail.com>
2021-06-22 17:17:01 +0100

    added a test file mongo.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
 
 app.use(cors());
 app.use(express.static('build'))
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms :res[content-length] :body - :req[content-length]'));
+
+// const mongoUri = 'mongodb+srv://ahmad:<password>@cluster0.cxs4b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 let persons = [
     {
---
+++ mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+console.log('argv', process.argv)
+if(process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://ahmad:${password}@cluster0.cxs4b.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+}).then()
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    number: String
+})
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+const contact = new Contact({
+    name: process.argv[3],
+    number: process.argv[4]
+})
+
+contact.save().then(res => {
+    console.log('contact saved', contact);
+    mongoose.connection.close()
+}).catch(err => console.log(err))
