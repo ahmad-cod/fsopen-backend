51aa3afe23908b09a941da883997a9f00bb9e313
aroyehunahmad <ahmadadewale800@gmail.com>
2021-06-23 07:53:39 +0100

    added eslint

--- models/contact.js
+++ models/contact.js
@@ -2,22 +2,22 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator')
 
 // DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-// const password = process.argv[2];
+// const password = process.argv[2]
 const url = process.env.MONGODB_URI;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const contactSchema = new mongoose.Schema({
   name: {
-      type: String,
-      required: true,
-      minLength: 3,
-      unique: true
+    type: String,
+    required: true,
+    minLength: 3,
+    unique: true
   },
   number: {
-      type: String,
-      required: true,
-      minLength: 8
+    type: String,
+    required: true,
+    minLength: 8
   }
 })
 
